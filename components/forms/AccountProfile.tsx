d87d6584ae57980a81fa9cac7d1e0f23603c45b5
Hem Pun <hemthegentlmen@gmail.com>
2023-08-08 14:31:35 +0545

    Fix:Image Uplad

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -52,10 +52,15 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   ) => {
     e.preventDefault();
     const fileReader = new FileReader();
-    if (e.target.files && e.target.files.length > 1) {
+    if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       setFiles(Array.from(e.target.files));
       if (!file.type.includes("image")) return;
+      fileReader.onload = async (e) => {
+        const imageDataUrl = e.target?.result?.toString() || "";
+        fieldChange(imageDataUrl);
+      };
+      fileReader.readAsDataURL(file);
     }
   };
   return (
